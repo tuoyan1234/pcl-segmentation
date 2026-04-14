25e5e8fd6b5790635884dc28a3b5b3af36521176
tyz <1872516355@qq.com>
2026-04-14 20:31:04 +0800

    fix build error

--- main.cpp
+++ main.cpp
@@ -1,3 +1,11 @@
+/*
+ * @Author: tyz 1872516355@qq.com
+ * @Date: 2026-04-13 22:21:35
+ * @LastEditors: tyz 1872516355@qq.com
+ * @LastEditTime: 2026-04-14 20:30:22
+ * @FilePath: /平面拟合/main.cpp
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 #include "segmentation.h"
 
 int main(){
